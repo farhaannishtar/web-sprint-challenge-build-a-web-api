c06ec8b12cec7a58b171eb114e316c0474bf72b8
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-05-14 08:41:05 -0400

    GET APIs functional

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,42 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model.js');
+const Actions = require('../actions/actions-model.js');
+
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('req: ', {req});
+  Projects.get()
+    .then(projects => {
+      console.log(projects);
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the projects',
+      });
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  Projects.get(req.params.id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        next({ status: 404, message: 'Project not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      next({message: 'Error retrieving the project'});
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -5,5 +5,24 @@ const server = express();
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+const actionsRouter = require('./actions/actions-router.js');
+const projectsRouter = require('./projects/projects-router.js');
+
+
+server.use(express.json());
+
+
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+server.use((err, req, res, next) => {
+  let { status = 500, message = 'unknown error occurred' } = err;
+  res.status(status).json({message: message});
+});
+
+server.use('*', (req, res) => {
+  // catch all 404 errors middleware
+  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+});
 
 module.exports = server;
